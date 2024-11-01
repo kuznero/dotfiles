c960be5d3cf75a6ced4b74c224449e83ecabf6e7
Roman Kuznetsov <roku@lix.one>
2024-11-01 21:26:42 +0100

    Configure barbar in NixVim

--- home-manager/programs/nixvim.nix
+++ home-manager/programs/nixvim.nix
@@ -70,6 +70,38 @@
 
     # ref: https://nix-community.github.io/nixvim/index.html
     plugins = {
+      barbar = {
+        enable = true;
+        settings = {
+          animation = true;
+          auto_hide = false;
+          clickable = true;
+          focus_on_close = "left";
+          insert_at_end = false;
+          insert_at_start = false;
+          maximum_length = 30;
+          maximum_padding = 2;
+          minimum_padding = 1;
+          no_name_title = "[No Name]";
+        };
+        keymaps = {
+          close.key = "<C-c>";
+          goTo1.key = "<C-1>";
+          goTo2.key = "<C-2>";
+          goTo3.key = "<C-3>";
+          goTo4.key = "<C-4>";
+          goTo5.key = "<C-5>";
+          goTo6.key = "<C-6>";
+          goTo7.key = "<C-7>";
+          goTo8.key = "<C-8>";
+          goTo9.key = "<C-9>";
+          last.key = "<C-0>";
+          previous.key = "<S-Tab>";
+          next.key = "<Tab>";
+          movePrevious.key = "<C-,>";
+          moveNext.key = "<C-.>";
+        };
+      };
       bufferline.enable = true;
       cmp-buffer.enable = true;
       cmp-nvim-lsp.enable = true;
