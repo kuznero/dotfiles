6d3918798d968bdb06243dcee627b4afce117237
Roman Kuznetsov <roku@lix.one>
2024-11-07 11:44:34 +0100

    Add podman* tools

--- home-manager/programs/common.nix
+++ home-manager/programs/common.nix
@@ -1,36 +1,37 @@
 { pkgs, pkgs-stable, system, ... }:
 
 {
-  home.packages = with pkgs; builtins.filter (pkg: pkg != null) [
-    btop
-    docker-credential-helpers
-    fd
-    ffmpegthumbnailer
-    fish
-    flux
-    gitFull
-    k9s
-    kitty
-    kubectl
-    lazydocker
-    lazygit
-    neofetch
-    nerdfonts
-    obsidian
-    poppler
-    ripgrep
-    rsync
-    (if system != "aarch64-linux" then slack else null)
-    (if system != "aarch64-linux" then spotify else null)
-    tmux
-    tree
-    vim
-    xclip
-    (if system != "aarch64-linux" then zoom-us else null)
-    zoxide
-    zsh
-  ] ++ (with pkgs-stable; [
-    yazi
-  ]);
+  home.packages = with pkgs;
+    builtins.filter (pkg: pkg != null) [
+      btop
+      docker-credential-helpers
+      fd
+      ffmpegthumbnailer
+      fish
+      flux
+      gitFull
+      k9s
+      kitty
+      kubectl
+      lazydocker
+      lazygit
+      neofetch
+      nerdfonts
+      obsidian
+      podman
+      podman-compose
+      podman-tui
+      poppler
+      ripgrep
+      rsync
+      (if system != "aarch64-linux" then slack else null)
+      (if system != "aarch64-linux" then spotify else null)
+      tmux
+      tree
+      vim
+      xclip
+      (if system != "aarch64-linux" then zoom-us else null)
+      zoxide
+      zsh
+    ] ++ (with pkgs-stable; [ yazi ]);
 }
-
