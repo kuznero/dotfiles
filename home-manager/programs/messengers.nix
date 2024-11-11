b5ac417d06417cb64fc91f9d3f6466d37828b022
Roman Kuznetsov <roku@lix.one>
2024-11-11 19:33:52 +0100

    Disable telegram on aarch64-darwin

--- home-manager/programs/messengers.nix
+++ home-manager/programs/messengers.nix
@@ -4,7 +4,7 @@
   home.packages = with pkgs;
     builtins.filter (pkg: pkg != null) [
       (if system != "aarch64-linux" then slack else null)
-      telegram-desktop
+      (if system != "aarch64-darwin" then telegram-desktop else null)
       (if system != "aarch64-linux" then zoom-us else null)
     ];
 }
