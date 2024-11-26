97f8e1407c86b3d3e959f6e443796c1cd62d69c7
Roman Kuznetsov <roku@lix.one>
2024-11-26 23:31:29 +0100

    Extend GitLab runners with shell runners

--- hosts/gitlab-runner.nix
+++ hosts/gitlab-runner.nix
@@ -5,9 +5,7 @@
     enable = true;
     clear-docker-cache.enable = true;
     clear-docker-cache.dates = "weekly";
-    settings = {
-      concurrent = 100;
-    };
+    settings = { concurrent = 100; };
     services = {
       sun = {
         # $ cat /data/gitlab-runner.conf
@@ -18,10 +16,16 @@
         limit = 0; # do not limit
         dockerImage = "alpine:3.18.5";
         dockerPrivileged = true;
-        dockerVolumes = [
-          "/var/run/docker.sock:/var/run/docker.sock"
-          "/data/cache"
-        ];
+        dockerVolumes =
+          [ "/var/run/docker.sock:/var/run/docker.sock" "/data/cache" ];
+      };
+      sun-shell = {
+        # $ cat /data/gitlab-runner.conf
+        # CI_SERVER_URL=https://gitlab.com
+        # CI_SERVER_TOKEN=glrt-***
+        authenticationTokenConfigFile = "/data/gitlab-runner.conf";
+        executor = "shell";
+        limit = 0; # do not limit
       };
     };
   };
