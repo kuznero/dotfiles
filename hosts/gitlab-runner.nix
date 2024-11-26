807779e17734e50afc0934377b8ed79518247f36
Roman Kuznetsov <roku@lix.one>
2024-11-26 23:41:08 +0100

    Turn sun into shell-only runner

--- hosts/gitlab-runner.nix
+++ hosts/gitlab-runner.nix
@@ -3,23 +3,9 @@
 {
   services.gitlab-runner = {
     enable = true;
-    clear-docker-cache.enable = true;
-    clear-docker-cache.dates = "weekly";
     settings = { concurrent = 100; };
     services = {
       sun = {
-        # $ cat /data/gitlab-runner.conf
-        # CI_SERVER_URL=https://gitlab.com
-        # CI_SERVER_TOKEN=glrt-***
-        authenticationTokenConfigFile = "/data/gitlab-runner.conf";
-        executor = "docker";
-        limit = 0; # do not limit
-        dockerImage = "alpine:3.18.5";
-        dockerPrivileged = true;
-        dockerVolumes =
-          [ "/var/run/docker.sock:/var/run/docker.sock" "/data/cache" ];
-      };
-      sun-shell = {
         # $ cat /data/gitlab-runner.conf
         # CI_SERVER_URL=https://gitlab.com
         # CI_SERVER_TOKEN=glrt-***
