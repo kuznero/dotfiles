7d7ab7c13a349c07beb78b67d18b4c1a8afe909e
Roman Kuznetsov <roku@lix.one>
2024-11-26 23:37:07 +0100

    Fix sun-shell gitlab runner

--- hosts/gitlab-runner.nix
+++ hosts/gitlab-runner.nix
@@ -23,7 +23,7 @@
         # $ cat /data/gitlab-runner.conf
         # CI_SERVER_URL=https://gitlab.com
         # CI_SERVER_TOKEN=glrt-***
-        authenticationTokenConfigFile = "/data/gitlab-runner.conf";
+        authenticationTokenConfigFile = "/data/gitlab-runner-shell.conf";
         executor = "shell";
         limit = 0; # do not limit
       };
