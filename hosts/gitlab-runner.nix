ceef8b111d794216e8d3c2c435eb770996728537
Roman Kuznetsov <roku@lix.one>
2024-11-26 23:43:27 +0100

    Fix

--- hosts/gitlab-runner.nix
+++ hosts/gitlab-runner.nix
@@ -9,7 +9,7 @@
         # $ cat /data/gitlab-runner.conf
         # CI_SERVER_URL=https://gitlab.com
         # CI_SERVER_TOKEN=glrt-***
-        authenticationTokenConfigFile = "/data/gitlab-runner-shell.conf";
+        authenticationTokenConfigFile = "/data/gitlab-runner.conf";
         executor = "shell";
         limit = 0; # do not limit
       };
