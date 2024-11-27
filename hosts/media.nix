4e7042295b816895a266210b8c5e9c7047a823f6
Roman Kuznetsov <roku@lix.one>
2024-11-27 16:53:41 +0100

    Extend media packages @moon

--- hosts/media.nix
+++ hosts/media.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ vlc ffmpeg-full ];
+  environment.systemPackages = with pkgs; [
+    ffmpeg-full
+    gimp-with-plugins
+    imagemagick
+    obs-studio
+    vlc
+  ];
 }
