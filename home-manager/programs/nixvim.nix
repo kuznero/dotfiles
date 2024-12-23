48baf5bb04b305696ac091a186af7a614c2fb660
Roman Kuznetsov <roku@lix.one>
2024-12-23 08:05:19 +0100

    Minor

--- home-manager/programs/nixvim.nix
+++ home-manager/programs/nixvim.nix
@@ -228,7 +228,28 @@
     ];
 
     editorconfig.enable = true;
-    colorschemes.catppuccin.enable = true;
+    colorschemes = {
+      # ayu.enable = true;
+      # base16.enable = true;
+      catppuccin.enable = true;
+      # cyberdream.enable = true;
+      # dracula-nvim.enable = true;
+      # everforest.enable = true;
+      # gruvbox.enable = true;
+      # kanagawa.enable = true;
+      # melange.enable = true;
+      # modus.enable = true;
+      # nightfox.enable = true;
+      # nord.enable = true;
+      # one.enable = true;
+      # onedark.enable = true;
+      # oxocarbon.enable = true;
+      # palette.enable = true;
+      # poimandres.enable = true;
+      # rose-pine.enable = true;
+      # tokyonight.enable = true;
+      # vscode.enable = true;
+    };
 
     opts = {
       autoindent = true;
