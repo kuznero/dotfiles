f3152871fb9d572917aa954772bdf5c31ffb6128
Roman Kuznetsov <roku@lix.one>
2025-01-03 12:34:41 +0100

    Update

--- home-manager/programs/tmux.nix
+++ home-manager/programs/tmux.nix
@@ -7,25 +7,6 @@
     enable = true;
     baseIndex = 1;
 
-    catppuccin = {
-      enable = true;
-      extraConfig = ''
-        set -g @catppuccin_flavor "macchiato"
-        set -g @catppuccin_status_background "none"
-
-        set -g @catppuccin_window_current_number_color "#{@thm_peach}"
-        set -g @catppuccin_window_current_text " #W"
-        set -g @catppuccin_window_current_text_color "#{@thm_bg}"
-        set -g @catppuccin_window_number_color "#{@thm_blue}"
-        set -g @catppuccin_window_text " #W"
-        set -g @catppuccin_window_text_color "#{@thm_surface_0}"
-        set -g @catppuccin_status_left_separator "█"
-
-        set -g status-right "#{E:@catppuccin_status_host}#{E:@catppuccin_status_date_time}"
-        set -g status-left ""
-      '';
-    };
-
     escapeTime = 10;
     historyLimit = 10000;
     keyMode = "vi";
@@ -50,4 +31,24 @@
       set-option -sg escape-time 10
     '';
   };
+
+  catppuccin.tmux = {
+    enable = true;
+    extraConfig = ''
+      set -g @catppuccin_flavor "macchiato"
+      set -g @catppuccin_status_background "none"
+
+      set -g @catppuccin_window_current_number_color "#{@thm_peach}"
+      set -g @catppuccin_window_current_text " #W"
+      set -g @catppuccin_window_current_text_color "#{@thm_bg}"
+      set -g @catppuccin_window_number_color "#{@thm_blue}"
+      set -g @catppuccin_window_text " #W"
+      set -g @catppuccin_window_text_color "#{@thm_surface_0}"
+      set -g @catppuccin_status_left_separator "█"
+
+      set -g status-right "#{E:@catppuccin_status_host}#{E:@catppuccin_status_date_time}"
+      set -g status-left ""
+    '';
+  };
+
 }
