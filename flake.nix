abd1b7cb42a96c1c2007d9150ad55217d94fbaa7
Roman Kuznetsov <roku@lix.one>
2025-01-07 10:36:01 +0000

    Start nix shell with custom zsh profile (example)

--- flake.nix
+++ flake.nix
@@ -402,5 +402,34 @@
           };
       };
 
+      # devShells = {
+      #
+      #   default = pkgs-unstable.mkShell {
+      #     hardeningDisable = [ "fortify" ];
+      #     buildInputs = dev-inputs;
+      #     shellHook = ''
+      #       temp_dir=$(mktemp -d)
+      #       cp $HOME/.zshenv $temp_dir/.zshenv || touch $temp_dir/.zshenv
+      #       cp $HOME/.zshrc $temp_dir/.zshrc || touch $temp_dir/.zshrc
+      #       chmod 0644 $temp_dir/.zshenv $temp_dir/.zshrc
+      #
+      #       export NIX_FLAKE_NAME="Nix DevEnv"
+      #       export PATH="$(pwd)/scripts:$PATH"
+      #
+      #       cat <<EOF >> $temp_dir/.zshrc
+      #       alias dev="echo dev"
+      #       # extend with custom aliases/functions/etc.
+      #       EOF
+      #
+      #       ZDOTDIR=$temp_dir exec ${pkgs-unstable.zsh}/bin/zsh
+      #     '';
+      #   };
+      #
+      #   ci = pkgs-unstable.mkShell {
+      #     hardeningDisable = [ "fortify" ];
+      #     buildInputs = dev-inputs;
+      #   };
+      #
+      # };
     };
 }
