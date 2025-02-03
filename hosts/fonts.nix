fa80abb5e6f68eaa0a7327314fd8dc022fd0aef6
Roman Kuznetsov <roku@lix.one>
2025-02-03 21:07:54 +0100

    Minor

--- hosts/fonts.nix
+++ hosts/fonts.nix
@@ -17,11 +17,11 @@
       iosevka
       monaspace
       montserrat
-      nerdfonts
+      # nerdfonts
       poppins
       source-sans-pro
       source-serif-pro
-      # (nerdfonts.override { fonts = [ "DroidSansMono" ]; })
+      (nerdfonts.override { fonts = [ "SpaceMono" ]; })
     ];
   };
 }
