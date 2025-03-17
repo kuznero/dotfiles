d8f3128e165184e44286f32cc57f9f9c95a246b4
Roman Kuznetsov <roku@lix.one>
2025-03-17 21:23:24 +0100

    Update font @moon home manager profile

--- flake.nix
+++ flake.nix
@@ -177,8 +177,8 @@
                 pkgs = nixpkgs-unstable.legacyPackages.${system};
                 system = system;
                 theme = "catppuccin-mocha";
-                fontFamily = "0xProto Nerd Font";
-                fontSize = "12";
+                fontFamily = "Mononoki Nerd Font";
+                fontSize = "14";
               })
               ./home-manager/programs/git.nix
               ./home-manager/programs/input-leap.nix
