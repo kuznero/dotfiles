ec2b7990cf6e39d0d583042ea29d37db9eb81ab2
Roman Kuznetsov <roku@lix.one>
2025-03-21 09:56:34 +0100

    Update flatpak.nix

--- hosts/flatpak.nix
+++ hosts/flatpak.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  # flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  # flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo --user
   # flatpak update
   # flatpak search xmind
   # flatpak install flathub net.xmind.XMind
