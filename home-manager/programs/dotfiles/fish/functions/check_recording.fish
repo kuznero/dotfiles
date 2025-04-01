1045da4d13ca27da8fa516f064fe80fc4655921d
Roman Kuznetsov <roku@lix.one>
2025-04-01 13:05:37 +0200

    Minor

--- home-manager/programs/dotfiles/fish/functions/check_recording.fish
+++ home-manager/programs/dotfiles/fish/functions/check_recording.fish
@@ -1,7 +1,6 @@
 function check_recording
-    set target_process wl-screenrec
-
-    if pgrep $target_process >/dev/null
-        echo "{\"text\":\"  \", \"tooltip\":\"Recording\", \"alt\":\"Recording\"}"
-    end
+  set target_process wl-screenrec
+  if pgrep $target_process >/dev/null
+    echo "{\"text\":\"  \", \"tooltip\":\"Recording\", \"alt\":\"Recording\"}"
+  end
 end
