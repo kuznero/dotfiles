f7fac8a38701adff0a622937fb75ca52e23a0c72
Roman Kuznetsov <roku@lix.one>
2025-04-01 09:18:13 +0200

    Update

--- hosts/hyprland.nix
+++ hosts/hyprland.nix
@@ -15,27 +15,46 @@
   };
 
   environment.systemPackages = with pkgs; [
+    at-spi2-atk
+    avizo
     brightnessctl
+    cliphist
     direnv
     dunst
+    ffmpeg_6-full
     fish
+    gifsicle
+    grim
     hyprcursor
     hypridle
     hyprlock
     hyprpaper
     hyprpicker
+    imagemagick
     kitty
     pamixer
     pavucontrol
     playerctl
+    poweralertd
+    psi-notify
     psmisc
     pyprland
+    qt6.qtwayland
     rofi
+    rofi-wayland
+    slurp
     starship
+    swappy
     waybar
     wezterm
+    wl-clip-persist
+    wl-clipboard
+    wl-screenrec
     wlogout
+    wlrctl
     wlsunset
     wofi
+    wtype
+    xdg-utils
   ];
 }
