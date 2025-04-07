d86bab34341590854548a24baa8f7bfc0b1a31b7
Roman Kuznetsov <roku@lix.one>
2025-04-07 14:23:08 +0200

    Add lock/screensaver for Xfce

--- hosts/xfce.nix
+++ hosts/xfce.nix
@@ -1,13 +1,18 @@
 { lib, pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs;
+    [
+      lightlocker # Session-locker for XFCE workaround
+    ];
+
   services.xserver = {
     enable = true;
     desktopManager = {
       xterm.enable = false;
       xfce = {
         enable = true;
-        enableScreensaver = false;
+        enableScreensaver = true;
       };
     };
     displayManager.lightdm.enable = true;
