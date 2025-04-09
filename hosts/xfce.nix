305eb81ea0b1561a51748b198eefedb7e5ba6968
Roman Kuznetsov <roku@lix.one>
2025-04-09 20:34:33 +0200

    Configure LightDM login screen resolution

--- hosts/xfce.nix
+++ hosts/xfce.nix
@@ -15,7 +15,12 @@
         enableScreensaver = true;
       };
     };
-    displayManager.lightdm.enable = true;
+    displayManager.lightdm = {
+      enable = true;
+      extraConfig = ''
+        display-setup-script=${pkgs.xorg.xrandr}/bin/xrandr --output eDP-1 --auto --output DP-3-1-8 --primary --left-of eDP-1 --auto
+      '';
+    };
   };
 
   services.displayManager.defaultSession = "xfce";
