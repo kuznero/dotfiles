ca5c0209fd522f8b67d64aed54747de249b56d64
Roman Kuznetsov <roku@lix.one>
2025-06-08 13:07:23 +0200

    Fix

--- hosts/onlyoffice-server.nix
+++ hosts/onlyoffice-server.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ onlyoffice-documentserver ];
+  services.onlyoffice = {
+    enable = true;
+    package = pkgs.onlyoffice-documentserver;
+    hostname = "0.0.0.0";
+  };
 }
