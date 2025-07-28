8794ccd304087df3f83af5dc2d1a6f2257e2fba8
Roman Kuznetsov <roku@lix.one>
2025-07-28 16:10:07 +0200

    Create mkdocs.nix

---
+++ home-manager/programs/mkdocs.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs;
+    [
+      (python3.withPackages (ps:
+        with ps; [
+          mkdocs
+          mkdocs-drawio-file
+          mkdocs-linkcheck
+          mkdocs-material
+          mkdocs-material-extensions
+          mkdocs-mermaid2-plugin
+          mkdocs-minify
+        ]))
+    ];
+}
