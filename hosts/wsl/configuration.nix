0360c12906a767f0713289d79837a758ee8ab06d
Roman Kuznetsov <roku@lix.one>
2025-09-08 10:31:27 +0200

    Enhance WSL configuration with interop settings

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -9,8 +9,37 @@
 
   programs.nix-ld.enable = true;
 
-  wsl.enable = true;
-  wsl.defaultUser = user;
+  wsl = {
+    enable = true;
+    defaultUser = user;
+    wslConf.interop.enabled = true;
+    wslConf.interop.appendWindowsPath = true;
+  };
+
+  # Fix WSL interop for Windows executables like VS Code
+  # This is a workaround for a known issue where NixOS's systemd-binfmt service removes
+  # the WSLInterop registration, breaking Windows binary execution (including VS Code).
+  #
+  # Related issues:
+  # - https://github.com/nix-community/NixOS-WSL/issues/552 - systemd-binfmt conflicts
+  # - https://github.com/nix-community/NixOS-WSL/issues/196 - boot.binfmt.emulatedSystems doesn't work
+  # - https://github.com/nix-community/NixOS-WSL/pull/64 - PR with binfmt/interop fixes
+  # - https://github.com/nix-community/NixOS-WSL/issues/238 - VSCode connection issues
+  # - https://github.com/nix-community/NixOS-WSL/issues/294 - VSCode setup documentation
+  #
+  # The proper solution would be to use: wsl.interop.register = true;
+  # However, this defaults to false due to global binfmt_misc state affecting all WSL distros.
+  # See: https://github.com/nix-community/NixOS-WSL/blob/main/modules/interop.nix
+  systemd.services.fix-wsl-interop = {
+    description = "Fix WSL Interop for Windows executables";
+    after = [ "systemd-binfmt.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.bash}/bin/bash -c 'echo :WSLInterop:M::MZ::/init:PF > /proc/sys/fs/binfmt_misc/register || true'";
+    };
+  };
 
   users.defaultUserShell = pkgs.zsh;
   programs.zsh.enable = true;
@@ -29,5 +58,8 @@
 
   security.sudo.extraConfig = ''Defaults env_keep += "NIX_SSL_CERT_FILE"'';
 
-  environment.systemPackages = with pkgs; [ vim ];
+  environment.systemPackages = with pkgs; [
+    vim
+    wslu  # WSL utilities including wslview for better Windows integration
+  ];
 }
