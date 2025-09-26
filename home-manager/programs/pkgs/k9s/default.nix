5a92582710983696600ebb862e741351808ae354
Roman Kuznetsov <roku@lix.one>
2025-09-26 11:18:35 +0200

    feat: Add k9s skins collection and switch to everforest-light theme

--- home-manager/programs/pkgs/k9s/default.nix
+++ home-manager/programs/pkgs/k9s/default.nix
@@ -3,13 +3,13 @@
 
 buildGoModule rec {
   pname = "k9s";
-  version = "0.50.9";
+  version = "0.50.12";
 
   src = fetchFromGitHub {
     owner = "derailed";
     repo = "k9s";
     rev = "v${version}";
-    hash = "sha256-wTQOBxJgrDPcWSiezCwHNgvfGa6oWBM+DNa7RC/9PJA=";
+    hash = "sha256-eN0z2Q6aMUcDnIyA1EoixKnDFvG7b7Z1F0LKZvV7pQo=";
   };
 
   ldflags = [
@@ -24,7 +24,7 @@ buildGoModule rec {
 
   proxyVendor = true;
 
-  vendorHash = "sha256-SDl47tAYiE00gFpCvoBeiV8XL/E29457b1RQW3pogmM=";
+  vendorHash = "sha256-Y0GBQf/iPhfBAlQGd/Ecn2fv0pspVzOTrmOzAluwfNY=";
 
   # TODO investigate why some config tests are failing
   doCheck = !(stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isAarch64);
