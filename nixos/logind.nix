d2621999546c6dc73f4ace9f5fd820f517205a1d
Roman Kuznetsov <roku@lix.one>
2025-12-14 20:47:14 +0100

    refactor(nixos): migrate to updated NixOS service option paths
    Update GNOME and logind configurations to use current NixOS option
    paths. Display manager and desktop manager options moved from
    services.xserver to services namespace. Logind lid switch options
    consolidated under services.logind.settings.Login.

--- nixos/logind.nix
+++ nixos/logind.nix
@@ -1,7 +1,9 @@
 { config, lib, nixpkgs, home-manager, ... }:
 
 {
-  services.logind.lidSwitch = "ignore";
-  services.logind.lidSwitchExternalPower = "ignore";
-  services.logind.lidSwitchDocked = "ignore";
+  services.logind.settings.Login = {
+    HandleLidSwitch = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+    HandleLidSwitchDocked = "ignore";
+  };
 }
