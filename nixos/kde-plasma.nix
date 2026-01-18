9419d9cea4a517b68ee3e692a9a0ff0639d1cee7
Roman Kuznetsov <roku@lix.one>
2026-01-18 16:33:43 +0100

    feat(nixos): add xdg-portal configuration for KDE Plasma
    Enable xdg-portal with KDE and GTK portals to improve desktop
    integration and support features like screen sharing and file dialogs.
    
    Signed-off-by: Roman Kuznetsov <roku@lix.one>

--- nixos/kde-plasma.nix
+++ nixos/kde-plasma.nix
@@ -8,4 +8,10 @@
   services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.settings.General.DisplayServer = "wayland";
   services.desktopManager.plasma6.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals =
+      [ pkgs.kdePackages.xdg-desktop-portal-kde pkgs.xdg-desktop-portal-gtk ];
+  };
 }
