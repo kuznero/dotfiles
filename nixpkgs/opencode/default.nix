ce99ad14751d39f026dddc691f29f6e5291c48a1
Roman Kuznetsov <roku@lix.one>
2026-03-09 09:29:58 +0100

    chore(nixpkgs): bump opencode to 1.2.22
    Signed-off-by: Roman Kuznetsov <roku@lix.one>

--- nixpkgs/opencode/default.nix
+++ nixpkgs/opencode/default.nix
@@ -15,12 +15,12 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "1.2.21";
+  version = "1.2.22";
   src = fetchFromGitHub {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-XCf0ijRZZjp16YS9V65ZMoQzTWYRUrle3L4vWIxvy3M=";
+    hash = "sha256-fSSXUPfvhlWb5YEtW+bbi2mJaOV4Cdx3hbp6lnysxuo=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -68,7 +68,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     dontFixup = true;
 
     # "sha256-lJ7lwElIvUAM7bKJQip4I5izA6GLGj+nZ9Lx3WgHmAI=";
-    outputHash = "sha256-bqhDicVdqBRjz4G9orfjzw5gf5im0wGCSgqa9FidMS8=";
+    outputHash = "sha256-+ouKn0xQv4UZy2MC5zgTu4QikranSdbUfqUo9nfpR40=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
