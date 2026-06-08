b33dfeea84cecdfb2585ed48310b95953464448d
Roman Kuznetsov <roku@lix.one>
2026-06-08 22:57:31 +0200

    feat(scripts): add greview helper and use opencode in gcauto

--- home/scripts.nix
+++ home/scripts.nix
@@ -3,6 +3,7 @@
 {
   home.packages = with pkgs; [
     (writeShellScriptBin "gcauto" (builtins.readFile ./scripts/gcauto))
+    (writeShellScriptBin "greview" (builtins.readFile ./scripts/greview))
     (writeShellScriptBin "gsreset" (builtins.readFile ./scripts/gsreset))
   ];
 }
