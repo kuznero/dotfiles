fa8bb7439fe89d143e2f9c13c2aca72b462ec74d
Roman Kuznetsov <roku@lix.one>
2026-07-21 09:20:02 +0200

    feat(tmux): use csi-u format for extended keys

--- home/tmux.nix
+++ home/tmux.nix
@@ -239,6 +239,7 @@ in {
       set -g focus-events on
 
       set -g extended-keys on
+      set -g extended-keys-format csi-u
 
       # Set default escape-time
       set-option -sg escape-time 10
